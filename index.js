db7ef6a686da9e1990850dec30521c64b7619cc2
Daniel Wehner <dawehner@googlemail.com>
2016-10-06 11:58:36 +0200

    run it every 30 minutes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var notify = require('osx-notifier');
- 
-// Stand up every 30 minutes at least.
-var duration = 30 * 60;
-notify({
-  type: 'fail',
-  title: 'Stand up',
-  subtitle: 'Standup, do some excerise or at least drink some water, dude!',
-  message: 'It is time for it',
-  group: 'taskdoer',
-});
+var cron = require('node-cron');
 
+// Stand up every 30 minutes at least.
+cron.schedule('* 30 * * *', () => {
+  notify({
+    type: 'fail',
+    title: 'Stand up',
+    subtitle: 'Standup, do some excerise or at least drink some water, dude!',
+    message: 'It is time for it',
+    group: 'taskdoer',
+  });
+});
